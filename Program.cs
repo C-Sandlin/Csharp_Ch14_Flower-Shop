b2c8a822418c284804a6ddf60657c522f8ae31b6
Colin Sandlin <csandlin010@gmail.com>
2019-07-16 10:22:25 -0500

    finished answering questions

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Flowershop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Flowershop myShop = new Flowershop();
+
+            foreach (IColor flower in myShop.BirthDayArrangement)
+            {
+                // BirthdayArrangement is a list of IColors. So, you can only access the ICOLOR properties. 
+                // can only do flower.Color. CANNOT do flower.StemLength, etc.
+
+                Console.WriteLine($"{flower.Color}");
+            }
+
+            // if you want to get list of roses, or only roses with certain props, etc., use LINQ methods
         }
     }
 }
